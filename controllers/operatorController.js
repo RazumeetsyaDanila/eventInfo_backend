66556499f7fce6ddd494d278461a11a7b19f4854
RazumeetsyaDanila <danil.kozhemyakin.99@mail.ru>
2021-06-13 12:39:59 +0300

    Исправлены запросы к БД

--- controllers/operatorController.js
+++ controllers/operatorController.js
@@ -5,15 +5,34 @@ const {Client, Order} = require('../models/models')
 class OperatorController {
 
     async clientadd(req, res) {
-        const {client_email, client_name, client_phone} = req.body;
-        const client = await Client.create({client_email, client_name, client_phone});
-        return res.json(client);
+        try {
+            const {client_email, client_name, client_phone} = req.body;
+            const client = await Client.create({client_email, client_name, client_phone});
+            return res.json({message: "Запись о клиенте успешно создана!"});
+        } catch (e) {
+            return res.json(e.message);
+        }
     }
 
     async orderadd(req, res) {
-        const {event_date, event_start_time, event_end_time, event_people_count, event_place, questQuestId, clientClientId, tariffTariffId} = req.body;
-        const order = await Order.create({event_date, event_start_time, event_end_time, event_people_count, event_place, questQuestId, clientClientId, tariffTariffId});
-        return res.json(order);
+        try {
+            const {event_name, event_date, event_start_time, event_end_time, event_people_count, event_place, questQuestId, clientClientId, tariffTariffId} = req.body;
+            const order = await Order.create({
+                event_name,
+                event_date,
+                event_start_time,
+                event_end_time,
+                event_people_count,
+                event_place,
+                event_status: "ЗАБРОНИРОВАНО",
+                questQuestId,
+                clientClientId,
+                tariffTariffId
+            });
+            return res.json({message: "Заказ добавлен!"});
+        } catch (e) {
+            return res.json(e.message);
+        }
     }
 
     async calculation(req, res, next) {
