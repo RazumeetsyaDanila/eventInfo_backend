80b0dd7fc2af285d4bcbed66bc623f7651f6beb0
DrPoseidon <a.popov.d@gmail.com>
2021-06-14 22:13:40 +0300

    внесены некоторые изменения

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,56 +1,70 @@
-const sequelize = require('../db');
-const {QueryTypes} = require('sequelize');
+const sequelize = require("../db");
+const { QueryTypes } = require("sequelize");
 
-const ApiError = require('../error/ApiError');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {User, Tariff, Order} = require('../models/models')
-
+const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { User, Tariff, Order } = require("../models/models");
 
 const generateJwt = (id, email, role) => {
-    return jwt.sign(
-        {id, email, role},
-        process.env.SECRET_KEY,
-        {expiresIn: '24h'}
-    )
-}
+  return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
 
 class UserController {
-
-    async login(req, res, next) {
-       const {email, password} = req.body
-        //console.log(req.body)
-        const hashPassword = await bcrypt.hash(password, 5)
-        const user = await User.findOne({where: {email, password}})
-        if (!user) {
-            res.status(404).send({message: "sdkn"})
-            // return next(ApiError.internal('Пользователь не найден'))
-        }
-        // let comparePassword = bcrypt.compareSync(password, user.password)
-        // if (!comparePassword) {
-        //     return next(ApiError.internal('Пользователь не найден'))
-        // }
-        console.log(password)
-        const token = generateJwt(user.id, user.email, user.role)
-        // console.log(token)
-        return res.status(200).send({token, user: {email, role: user.role}})
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email, password } });
+    if (user) {
+      const token = generateJwt(
+        user.dataValues.id,
+        email,
+        user.dataValues.role
+      );
+      res.status(200).send({
+        token,
+        user: {
+          id: user.dataValues.id,
+          name: user.dataValues.name,
+          email: email,
+          role: user.dataValues.role,
+        },
+      });
+    } else {
+      res.sendStatus(404);
     }
+  }
 
-    async orders(req, res) {
-        const orders = await sequelize.query("SELECT event_date, event_start_time, event_end_time, " +
-            "event_people_count, event_place, event_name, event_status FROM orders", { type: QueryTypes.SELECT});
-        return res.json(orders);
+  async orders(req, res) {
+    const orders = await sequelize.query(
+      "SELECT u.name, o.order_id, o.event_date, o.event_start_time, o.event_end_time, " +
+        "o.event_people_count, o.event_place, o.event_name, o.event_status FROM orders o " +
+        "LEFT JOIN user_orders u_o ON (u_o.order_id = o.order_id)" +
+        "LEFT JOIN users u ON (u.id = u_o.user_id)",
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+    if (orders.length) {
+      res.status(200).send({ orders });
+    } else {
+      res.sendStatus(404);
     }
+  }
 
-    async tariffs(req, res) {
-        const tariffs = await sequelize.query("SELECT tariff_name, tariff_price, salary_worker FROM tariffs", { type: QueryTypes.SELECT});
-        return res.json(tariffs);
-    }
+  async tariffs(req, res) {
+    const tariffs = await sequelize.query(
+      "SELECT tariff_name, tariff_price, salary_worker FROM tariffs",
+      { type: QueryTypes.SELECT }
+    );
+    return res.json(tariffs);
+  }
 
-    async check(req, res) {
-        const token = generateJwt(req.user.id, req.user.email, req.user.role)
-        return res.json({token})
-    }
+  async check(req, res) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
+  }
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
--- routes/managerRouter.js
+++ routes/managerRouter.js
@@ -1,18 +1,34 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const managerController = require('../controllers/managerController');
-const checkRole = require('../middleware/checkRoleMiddleware');
+const managerController = require("../controllers/managerController");
+const checkRole = require("../middleware/checkRoleMiddleware");
 
-router.post('/tariffadd', checkRole("MANAGER"), managerController.tariffadd);
-router.post('/assignment', checkRole("MANAGER"), managerController.assignment);
-router.post('/registration', checkRole("MANAGER"), managerController.registration);
-router.get('/unprocessedpursh', checkRole("MANAGER"), managerController.unprocessedpursh);
-router.post('/completepurchase', checkRole("MANAGER"), managerController.completepurchase);
-router.get('/purchases', checkRole("MANAGER"), managerController.purchases);
-router.get('/requisite', checkRole("MANAGER"), managerController.requisite);
-router.get('/report', checkRole("MANAGER"), );
-router.get('/workers', checkRole("MANAGER"), managerController.workers);
-router.get('/upcomingworks', checkRole("MANAGER"), managerController.upcomingworks);
-router.post('/completeworks', checkRole("MANAGER"), managerController.completeworks);
+router.post("/tariffadd", checkRole("MANAGER"), managerController.tariffadd);
+router.post("/assignment", managerController.assignment);
+router.post(
+  "/registration",
+  checkRole("MANAGER"),
+  managerController.registration
+);
+router.get("/unprocessedpursh", managerController.unprocessedpursh);
+router.post(
+  "/completepurchase",
+  checkRole("MANAGER"),
+  managerController.completepurchase
+);
+router.get("/purchases", checkRole("MANAGER"), managerController.purchases);
+router.get("/requisite", checkRole("MANAGER"), managerController.requisite);
+router.get("/report", checkRole("MANAGER"));
+router.get("/workers", managerController.workers);
+router.get(
+  "/upcomingworks",
+  checkRole("MANAGER"),
+  managerController.upcomingworks
+);
+router.post(
+  "/completeworks",
+  checkRole("MANAGER"),
+  managerController.completeworks
+);
 
 module.exports = router;
